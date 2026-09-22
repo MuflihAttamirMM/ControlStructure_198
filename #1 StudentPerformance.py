b99216bfe4a385874e785b4815d93bc22ad94403
Muflay <muflihattamirmuslim@gmail.com>
2026-09-22 10:40:03 +0700

    Tolong Jaringannya Jelek

---
+++ #1 StudentPerformance.py
@@ -0,0 +1,12 @@
+performance = float(input("Masukkan nilai performance: "))
+
+if performance >= 90:
+    print("Excellent Performance")
+elif performance  >= 80:
+    print("Very Good Performance")
+elif performance >= 70:
+    print("Good Performance")
+elif performance >= 60:
+    print ("Average performance")
+else:
+    print("Below Average performance")
